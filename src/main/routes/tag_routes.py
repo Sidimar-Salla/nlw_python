4cdd095f07c449f511a4e6ace687b055149feb51
Sidimar Salla <sidimarsalla0@gmail.com>
2024-02-06 18:59:01 -0300

    feature Implementando view for tag creador

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, jsonify
-
+from flask import Blueprint, jsonify, request
+from src.views.http_types.http_request import HttpRequest
+from src.views.tag_creator_view import TagCreatorView
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=['POST'])
 def create_tags():
-    print('Olá, Mundo!')
-    return jsonify({ 'opa': 'funcionou' }), 200
+    tag_creator_view = TagCreatorView()
+    https_request = HttpRequest(body=request.json)
+
+    response = tag_creator_view.validate_and_create(https_request)
+
+    return jsonify(response.body), response.status_code
